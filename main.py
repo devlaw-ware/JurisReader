0d0d2217e80e4d2fb4da5c1e30ef6dcf46fbee45
devlaw-ware <devlaw.ware@gmnail.com>
2026-05-13 15:08:33 -0300

    fix: lines

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import sys
 
 from PySide6.QtWidgets import QApplication
-
 from software.ui.main_window import MainWindow
 
-
 app = QApplication(sys.argv)
-
 window = MainWindow()
 window.show()
-
 app.exec()
