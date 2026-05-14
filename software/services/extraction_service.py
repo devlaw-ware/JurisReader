3283388b0b9df2b7894ccbad87ad85e5c286f707
devlaw-ware <devlaw.ware@gmnail.com>
2026-05-14 16:34:17 -0300

    feat: integrate regex extraction into interface

--- software/services/extraction_service.py
+++ software/services/extraction_service.py
@@ -1,3 +1,21 @@
-importe re 
+import re
 
-def extract_dat
+def extract_dates(text):
+
+    dates = re.findall(
+        r'\d{2}/\d{2}/\d{4}',
+        text
+    )
+
+    return dates
+
+
+def extract_values(text):
+
+    values = re.findall(
+        r'R\$\s?[\d\.,]+',
+
+        text
+    )
+
+    return values
--- software/ui/main_window.py
+++ software/ui/main_window.py
@@ -6,6 +6,7 @@ from PySide6.QtWidgets import (
     QFileDialog
 )
 
+from software.services.extraction_service import extract_dates, extract_values
 from software.services.pdf_service import extract_text
 from software.services.ai_service import summarize_text 
 
@@ -22,12 +23,14 @@ class MainWindow(QWidget):
         self.summary_button = QPushButton("Gerar Resumo do Processo")  #Button create to generate summary of the text extracted from the pdf file
 
         self.text_area = QTextEdit()
+        self.info_area = QTextEdit()
 
         layout = QVBoxLayout()
 
         layout.addWidget(self.button)
         layout.addWidget(self.summary_button) #Add the summary button to the layout
         layout.addWidget(self.text_area)
+        layout.addWidget(self.info_area)
 
         self.setLayout(layout)
 
@@ -36,23 +39,37 @@ class MainWindow(QWidget):
 
     def open_pdf(self):
         
-        
-
         file_path, _ = QFileDialog.getOpenFileName(
             self,
             "Selecionar PDF",
             "",
             "PDF Files (*.pdf)"
         )
+        
 
         if file_path:
 
             text = extract_text(file_path)
             self.current_text = text #Store the extracted text in a variable to be used later for generating the summary
             self.text_area.setText(text)
+            dates = extract_dates(text)
+            values = extract_values(text)
+            
+            info_text = f"""
+DATAS:
+
+{'\n'.join(dates)}
+
+VALORES:
+
+{'\n'.join(values)}
+"""
+
+        self.info_area.setText(info_text)
 
     def generate_summary(self):
 
         if hasattr(self, 'current_text'):
             summary = summarize_text(self.current_text) #Generate the summary using the current text extracted from the pdf file
-            self.text_area.setText(summary) #Display the summary in the text area
+            self.text_area.setText(summary) #Display the summary in the text areas
+    
