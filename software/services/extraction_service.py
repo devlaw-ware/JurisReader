57c1624b7e0c732dd1bcc0c87ae5060c00b4bde2
devlaw-ware <devlaw.ware@gmnail.com>
2026-05-24 20:51:36 -0300

    feat: implement CNJ process number recognition

--- software/services/extraction_service.py
+++ software/services/extraction_service.py
@@ -19,3 +19,12 @@ def extract_values(text):
     )
 
     return values
+
+def extract_process_number(text): #funçao para extrair o número do processo judicial do texto 
+
+    process_number = re.findall( #procura padrões no texto que correspondam ao formato do número do processo judicial
+        r'\d{7}-\d{2}\.\d{4}\.\d\.\d{2}\.\d{4}',
+        text\
+    )
+    
+    return list(set(process_number))
